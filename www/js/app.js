a727e3b029e4a462677bd1ffb586b5e076b1169a
Danny DeBelius <danny.debelius@gmail.com>
2014-12-17 16:39:00 -0500

    Stub out play button, poster video

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,15 @@
 // Global jQuery references
 var $shareModal = null;
 var $commentCount = null;
+var $audioPlayer = null;
+var $currentTime = null;
+var $play = null;
+var $pause = null;
+var $goButton = null;
 
 // Global state
 var firstShareLoad = true;
+var audioFile = '/assets/audio/naught-or-nice-1-v121014.mp3';
 
 /*
  * Run on page load.
@@ -12,10 +18,18 @@ var onDocumentLoad = function(e) {
     // Cache jQuery references
     $shareModal = $('#share-modal');
     $commentCount = $('.comment-count');
+    $audioPlayer = $('#audio-player');
+    $currentTime = $('.current-time');
+    $play = $('.play');
+    $pause = $('.pause');
+    $goButton = $('.js-go');
 
     // Bind events
     $shareModal.on('shown.bs.modal', onShareModalShown);
     $shareModal.on('hidden.bs.modal', onShareModalHidden);
+    $play.on('click', onPlayClick);
+    $pause.on('click', onPauseClick);
+    $goButton.on('click', onGoClick);
 
     // configure ZeroClipboard on share panel
     ZeroClipboard.config({ swfPath: 'js/lib/ZeroClipboard.swf' });
@@ -25,23 +39,79 @@ var onDocumentLoad = function(e) {
         clippy.on('aftercopy', onClippyCopy);
     });
 
-    renderExampleTemplate();
     getCommentCount(showCommentCount);
+    setupAudio();
+    $('.covervid-video').coverVid(640, 360);
 }
 
 /*
- * Basic templating example.
+ * Configure jPlayer.
  */
-var renderExampleTemplate = function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
+var setupAudio = function() {
+    $audioPlayer.jPlayer({
+        ended: onAudioEnded,
+        supplied: 'mp3',
+        loop: false,
+        timeupdate: onTimeUpdate,
+        swfPath: APP_CONFIG.S3_BASE_URL + '/js/lib/jquery.jplayer.swf'
     });
 
-    var html = JST.example(context);
+    $audioPlayer.jPlayer('setMedia', {
+        mp3: APP_CONFIG.S3_BASE_URL + audioFile
+    });
+}
 
-    $('#template-example').html(html);
+var onAudioEnded = function(e) {
+    var time = e.jPlayer.status.currentTime;
+
+    if (time != 0 && time != e.jPlayer.status.duration) {
+        // End fired prematurely
+        console.log(e.jPlayer.status.currentTime);
+        console.log(e.jPlayer.status.currentPercentAbsolute);
+        console.log(e.jPlayer.status.currentPercentRelative);
+        console.log(e.jPlayer.status.duration);
+    }
+}
+
+/*
+ * Update playback timer display.
+ */
+var onTimeUpdate = function(e) {
+    var time_text = $.jPlayer.convertTime(e.jPlayer.status.currentTime);
+    $currentTime.text(time_text);
+};
+
+/*
+ *  Open the card
+ */
+var onGoClick = function(e) {
+    e.preventDefault();
+    $audioPlayer.jPlayer('play');
+    $play.hide();
+    $pause.show();
+    $('.player').addClass('slide-in');
+    $('.slide-card-closed .content-wrapper').fadeOut();
+    $('.covervid-wrapper').css('opacity', 1);
+}
+
+/*
+ * Play the song, show the pause button
+ */
+var onPlayClick = function(e) {
+    e.preventDefault();
+    $audioPlayer.jPlayer('play');
+    $play.hide();
+    $pause.show();
+}
+
+/*
+ * Pause the song, show the play button
+ */
+var onPauseClick = function(e) {
+    e.preventDefault();
+    $audioPlayer.jPlayer('pause');
+    $pause.hide();
+    $play.show();
 }
 
 /*
